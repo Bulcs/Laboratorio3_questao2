a4d4b633b92c51953dc394dbd00696bcaae16ad3
Vinícius Bulcão <vrbulcao@hotmail.com>
2018-05-11 23:46:32 -0300

    Add files via upload
    Protótipo

---
+++ questao2/saque.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+float Saque::setSaqueEspecial(float saque){
+	cin >> saque;
+	return saque;
+}
+
+float Saque::setSaquePadrao(float saque){
+	cin >> saque;
+	return saque;
+}
